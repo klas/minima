625d2d63b59fdc0e42324b0ac096cd7c74adccb0
klas <klas.berlic@gmail.com>
2011-04-29 16:55:57 +0200

    com_menus items batch

--- html/com_menus/items/default_batch.php
+++ html/com_menus/items/default_batch.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version		$Id: default_batch.php 20196 2011-01-09 02:40:25Z ian $
+ * @version		$Id$
  * @package		Joomla.Administrator
  * @subpackage	com_menus
  * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
@@ -18,11 +18,12 @@ $published = $this->state->get('filter.published');
 ?>
 <fieldset class="batch">
 	<legend><?php echo JText::_('COM_MENUS_BATCH_OPTIONS');?></legend>
+	<?php if (!defined('MOLAJO_ACL')) : ?>
 	<label id="batch-access-lbl" for="batch-access" class="hasTip" title="<?php echo JText::_('JGLOBAL_BATCH_ACCESS_LABEL').'::'.JText::_('JGLOBAL_BATCH_ACCESS_LABEL_DESC'); ?>">
 		<?php echo JText::_('JGLOBAL_BATCH_ACCESS_LABEL') ?>
 	</label>
 	<?php echo JHtml::_('access.assetgrouplist', 'batch[assetgroup_id]', '', 'class="inputbox"', array('title' => JText::_('JGLOBAL_BATCH_NOCHANGE'), 'id' => 'batch-access'));?>
-
+	<?php endif; ?>
 	<?php if ($published >= 0) : ?>
 		<label id="batch-choose-action-lbl" for="batch-choose-action">
 			<?php echo JText::_('COM_MENUS_BATCH_MENU_LABEL'); ?>
@@ -32,7 +33,7 @@ $published = $this->state->get('filter.published');
 				<option value=""><?php echo JText::_('JSELECT') ?></option>
 				<?php echo JHtml::_('select.options', JHtml::_('menu.menuitems', array('published' => $published)));?>
 			</select>
-			<?php echo JHTML::_( 'select.radiolist', $options, 'batch[move_copy]', '', 'value', 'text', 'm'); ?>
+			<?php echo JHtml::_( 'select.radiolist', $options, 'batch[move_copy]', '', 'value', 'text', 'm'); ?>
 		</fieldset>
 	<?php endif; ?>
 	<button type="submit" onclick="Joomla.submitbutton('item.batch');">
